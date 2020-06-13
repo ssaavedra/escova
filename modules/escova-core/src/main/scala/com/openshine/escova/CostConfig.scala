4f6c7e8c9b806544457be7d8a47173d896611f0c
Santiago Saavedra <info@ssaavedra.eu>
2020-06-13 16:23:12 +0200

    replace all usages of whitelist/blacklist
    There's been plenty of discussion on the usage of these terms in tech.
    Since allowlist and blocklist are more self-explanator than their
    counterparts, this change has negative cost implications.
    
    However, it breaks current API compatibility due to the naming change.
    
    It should not break ABI compatibility.

--- modules/escova-core/src/main/scala/com/openshine/escova/CostConfig.scala
+++ modules/escova-core/src/main/scala/com/openshine/escova/CostConfig.scala
@@ -9,8 +9,8 @@ import com.openshine.escova.nodes.TreeNode
   * configuration.
   *
   * @param default the default configuration for non-customized node types
-  * @param custom a map of custom configuration for certain node types. Keys
-  *               should be the name of the aggregates
+  * @param custom  a map of custom configuration for certain node types. Keys
+  *                should be the name of the aggregates
   */
 case class CostConfig(
     default: NodeCostConfig = NodeCostConfig(),
@@ -19,9 +19,9 @@ case class CostConfig(
 
 object CostConfig {
 
-  /** A default cost config is provided here in case no other is available
-    */
-  implicit val defaultCostConfig: CostConfig = CostConfig()
+    /** A default cost config is provided here in case no other is available
+      */
+    implicit val defaultCostConfig: CostConfig = CostConfig()
 }
 
 case class NodeCostConfig(
@@ -31,47 +31,53 @@ case class NodeCostConfig(
       * calculate their cost from the seconds requested
       */
     defaultNodeCost: Double = 1d,
+
     /** Cost per child of the current node
       */
     perChildCost: Double = 10d,
+
     /** Operation to aggregate the children costs to the current node's costs.
       */
     childrenCostOp: String = "mul",
+
     /** Operation to aggregate all the children costs together.
       *
       * Usually, this should be the sum or the average cost.
       */
     nodeCostOpAggChild: String = "sum",
+
     /** Custom configuration for certain types of nodes.
       *
       * This may be allowed in code paths for certain node types
       */
     customNodeConfig: Map[String, String] = Map(),
+
     /** The maximum number of allowed subaggregates from this node downwards.
       */
     maxTreeHeight: Int = 4,
+
     /** If non-empty, only these type of children will be allowed as subaggregates.
       */
-    whitelistChildren: List[String] = List(),
+    allowlistChildren: List[String] = List(),
+
     /** If non-empty these types of children will be disallowed as subaggregates.
       */
-    blacklistChildren: List[String] = List(),
+    blocklistChildren: List[String] = List(),
 ) {
+    /** Determine whether a TreeNode fits the allow/block lists.
+      *
+      * Take care also of the max height allowed.
+      *
+      * @param t the TreeNode to include
+      * @return whether the children is allowed at the current point
+      */
+    def isChildAllowed(t: TreeNode): Boolean = {
+        val allowlisted =
+            if (allowlistChildren.nonEmpty)
+                allowlistChildren.contains(t.node.getType)
+            else
+                !blocklistChildren.contains(t.node.getType)
 
-  /** Determine whether a TreeNode fits the white/black lists.
-    *
-    * Take care also of the max height allowed.
-    *
-    * @param t the TreeNode to inlclude
-    * @return whether the children is allowed at the current point
-    */
-  def isChildAllowed(t: TreeNode): Boolean = {
-    val whitelisted =
-      if (whitelistChildren.nonEmpty)
-        whitelistChildren.contains(t.node.getType)
-      else
-        !blacklistChildren.contains(t.node.getType)
-
-    whitelisted && t.height < maxTreeHeight
-  }
+        allowlisted && t.height < maxTreeHeight
+    }
 }
